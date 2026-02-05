77465a09c0e6698d56e5851ab920a2f7719d81bc
Fabio Kayser <f.kayser@oraylis.de>
2026-02-05 10:55:50 +0100

    fix(build): drop pkg_resources dependency

--- pyinstaller/datam8_entrypoint.py
+++ pyinstaller/datam8_entrypoint.py
@@ -1,18 +1,5 @@
 from __future__ import annotations
 
-import warnings
-
-warnings.filterwarnings(
-    "ignore",
-    message=r"pkg_resources is deprecated as an API\\..*",
-    category=UserWarning,
-)
-warnings.filterwarnings(
-    "ignore",
-    message=r'Field name "schema" in "TableMetadataBody" shadows an attribute in parent "DataSourceAuthBody".*',
-    category=UserWarning,
-)
-
 from datam8.app import app
 
 
