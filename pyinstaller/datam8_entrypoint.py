b50dc0ffe5b22392452ed25d7ba8153b5d0072dc
Fabio Kayser <f.kayser@oraylis.de>
2026-02-05 10:05:57 +0100

    fix(server): CORS preflight + silence warnings

--- pyinstaller/datam8_entrypoint.py
+++ pyinstaller/datam8_entrypoint.py
@@ -1,5 +1,18 @@
 from __future__ import annotations
 
+import warnings
+
+warnings.filterwarnings(
+    "ignore",
+    message=r"pkg_resources is deprecated as an API\\..*",
+    category=UserWarning,
+)
+warnings.filterwarnings(
+    "ignore",
+    message=r'Field name "schema" in "TableMetadataBody" shadows an attribute in parent "DataSourceAuthBody".*',
+    category=UserWarning,
+)
+
 from datam8.app import app
 
 
@@ -9,4 +22,3 @@ def main() -> None:
 
 if __name__ == "__main__":
     main()
-
