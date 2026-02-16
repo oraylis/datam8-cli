dbb2fcb65a5a639e9110606e2ad93c10ee3dc307
Fabio Kayser <f.kayser@oraylis.de>
2026-02-16 10:10:30 +0100

    Fix

--- src/datam8/api/routes/api_solution.py
+++ src/datam8/api/routes/api_solution.py
@@ -121,7 +121,6 @@ async def solution_info(path: str | None = Query(None)) -> SolutionInfoResponse:
 @router.get(
     "/solution/full",
     response_model_exclude_unset=True,
-    response_model_exclude_defaults=True,
     response_model_exclude_none=True,
 )
 async def solution_full(path: str | None = Query(None)) -> SolutionFullResponse:
