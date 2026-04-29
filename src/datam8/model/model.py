643f2d296ab71c5825785f020f09bb7efef05f0c
fakayser <141181005+fakayser@users.noreply.github.com>
2026-04-29 08:34:00 +0200

    FI from main (#33)
    * patch(migration): data sources without extendedProperties (#31)
    
    * deps: update according to dependabot (#32)
    
    ---------
    
    Co-authored-by: matschi2 <mail@matschi2.dev>

--- src/datam8/model/model.py
+++ src/datam8/model/model.py
@@ -389,7 +389,7 @@ class Model:
             raise utils.create_error(errors.InvalidLocatorError(str(wrapper.locator)))
 
         for zone in self.zones.values():
-            if zone.entity.localFolderName == wrapper.locator.folders[0]:
+            if zone.entity.localFolderName or zone.entity.name == wrapper.locator.folders[0]:
                 return zone
 
         raise utils.create_error(errors.EntityNotFoundError(f"zones/{wrapper.locator.folders[0]}"))
@@ -888,6 +888,7 @@ class Model:
             raise utils.create_error(errors.InvalidLocatorError(str(locator)))
 
         property_value = self.get_entity_by_locator(locator)
+
         results = [
             wrapper
             for wrapper in self.get_entities(model_locator)
